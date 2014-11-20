3fa1d7f7c7fd4ccc0059e7c1007909d1189e2140
Khoa Pham <khoa.pham@me.com>
2014-11-20 15:33:11 -0800

    Init content & function.

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,3 +1,16 @@
 jQuery(document).ready(function($) {
+  var HeaderOnScroll = function() {
+    this.init();
+  }
+  
+  HeaderOnScroll.prototype = {
+    init: function() {
+      this.getDocumentHeight();
+    },
+
+    getDocumentHeight: function() {
+      return $( document ).height();
+    }
+  }
 
 });
