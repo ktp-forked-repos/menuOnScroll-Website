465f2ea47a580922d4ce3e0bad25f375fb5492f4
Khoa Pham <khoa.pham@me.com>
2014-11-20 22:00:08 -0800

    Add several function to calculate the height & container.

--- Gemfile
+++ Gemfile
@@ -1,19 +1,11 @@
-# If you have OpenSSL installed, we recommend updating
-# the following line to use "https"
 source 'http://rubygems.org'
-
 gem "middleman", "~>3.2.0"
-
-
-# Live-reloading plugin
 gem "middleman-livereload", "~> 3.1.0"
-
-
-gem 'asset_sync'
-gem 'middleman-s3_sync'
+gem "asset_sync"
+gem "middleman-s3_sync"
 gem "middleman-bower"
-gem 'bourbon'
-
+gem "bourbon"
+gem "neat"
 
 # For faster file watcher updates on Windows:
 gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,16 +1,48 @@
 jQuery(document).ready(function($) {
   var HeaderOnScroll = function() {
-    this.init();
+    this.container;
+    this.menus = new Array();
+    this.containerSections = new Array();
+
+    this.setContainer = function(container) { this.container = container; }
+    this.getContainer = function() { return this.container; }
+
   }
   
   HeaderOnScroll.prototype = {
     init: function() {
       this.getDocumentHeight();
+      this.setMenuItems($(".menu-item"));
+      this.setContainerSections();
     },
 
     getDocumentHeight: function() {
       return $( document ).height();
+    },
+
+    getContainerHeight: function(container) {
+      console.log (container.height());
+      return $(container).outerHeight();
+    },
+
+    getSectionId: function(menu) {
+      return $(menu).find("a").attr("href");
+    },
+
+    setMenuItems: function($menus) {
+      for (var i = 0; i < $menus.length; i++ ) {
+        this.menus[i] = $menus[i]; 
+      }
+    },
+
+    setContainerSections: function() {
+      for (var i = 0; i < this.menus.length; i++) {
+        this.containerSections[i] =  this.getSectionId(this.menus[i]);
+      }
+      console.log(this.containerSections);
     }
   }
 
+  var headerOnScroll = new HeaderOnScroll();
+  headerOnScroll.init();
 });
